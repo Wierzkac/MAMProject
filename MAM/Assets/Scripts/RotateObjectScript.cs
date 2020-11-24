fb4d06cc073b7c365d2f1ad782bd9b8f970d7e2b
Wierzkac <wierzkac@gmail.com>
2020-11-24 23:26:29 +0100

    InitialCommit

---
+++ MAM/Assets/Scripts/RotateObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject obiekt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        obiekt.transform.Rotate(1f, 0, 0, Space.World);
+    }
+}
