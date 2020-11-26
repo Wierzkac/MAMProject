1f142c151876c0d63d5a0d0ce54098f567e501a5
Wierzkac <wierzkac@gmail.com>
2020-11-26 23:58:33 +0100

    AddedAndUpdatedAllTasks

---
+++ Assets/Scripts/ShowFPSScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFPSScript : MonoBehaviour
+{
+    [SerializeField]
+    Text display_Text;
+
+    public void Update()
+    {
+        float current = 0;
+        current = Time.frameCount / Time.time;
+        display_Text.text = current.ToString() + " FPS";
+    }
+}
---
+++ Assets/Scripts/SunMovingScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovingScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToRotateAround;
+    [SerializeField]
+    float theta = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(objectToRotateAround.transform.position, Vector3.right, theta);
+    }
+}
